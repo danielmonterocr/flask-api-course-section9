1b9dc9072ba492046308680dd2f3badd2f0fceb7
Daniel Montero <danielmontero.cr@gmail.com>
2020-11-23 14:40:24 -0600

    Change use of id variable

--- models/item.py
+++ models/item.py
@@ -3,11 +3,11 @@ from db import db
 class ItemModel(db.Model):
     __tablename__ = 'items'
 
-    id = db.Column(db.Integer, primary_key=True)
+    _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store_id = db.Column(db.Integer, db.ForeignKey('stores._id'))
     store = db.relationship('StoreModel')
 
     def __init__(self, name, price, store_id):
